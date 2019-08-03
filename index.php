a179e145381ed67e2a30830ab3d29602d21e0863
Burt Zheng <504779282@qq.com>
2019-08-03 17:13:58 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// 应用目录为当前目录
+// Bird：定义当前目录
+define('APP_PATH',__DIR__.'/');
+
+
+
+// 开启调试模式
+define('APP_DEBUG',true);
+
+
+// 加载框架文件
+// Bird： 加载内核文件
+require(APP_PATH.'fastphp/Fastphp.php');
+
+
+// 加载配置文件
+$config = require(APP_PATH.'config/config.php');
+
+
+// 实例化框架类
+(new fastphp\Fastphp($config)) ->run();
