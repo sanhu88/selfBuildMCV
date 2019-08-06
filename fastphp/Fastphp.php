5575ddb71d4dad451120ea2ff79eb195d4fec06e
Burt Zheng <504779282@qq.com>
2019-08-06 21:07:02 +0800

    complete router function

--- fastphp/Fastphp.php
+++ fastphp/Fastphp.php
@@ -54,9 +54,31 @@ class Fastphp{
 			$controllerName = ucfirst($urlArray[0]);
 
 			//array_shift() 函数删除数组中第一个元素，并返回被删除元素的值。
-			arrary_shift($urlArray);
+			array_shift($urlArray);
 
 			$actionName = $urlArray ? $urlArray[0] : $actionName;
+
+			array_shift($urlArray);
+
+			$param = $urlArray ? $urlArray : array();
 		}
+
+		//判断控制器和操作是否存在
+		$controller = 'app\\controllers\\'.$controllerName.'Contreller';
+
+		if(class_exists($controller)){
+			exit($controller.'控制器不存在！');
+		}	
+		if(method_exists($controller, $actionName)){
+			exit($actionName.'方法不存在！！');
+		}
+
+
+		$dispatch = new $controller($controllerName,$actionName);
+
+
+		call_user_func_array(array($dispatch,$ac), $param);
+		//上面一行等同于 $dispatch->$actionName($param)
+		//call_user_func_array — 调用回调函数，并把一个数组参数作为回调函数的参数
 	}
 }
