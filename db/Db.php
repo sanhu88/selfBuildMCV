e98ff581e3a94cd9ba08ebdfd5d79a0cfc8200fa
Burt Zheng <504779282@qq.com>
2019-08-21 22:40:11 +0800

    complet db/Db.php

---
+++ db/Db.php
@@ -0,0 +1,27 @@
+<?php
+namespace fastphp\db;
+
+use PDO;
+use PDOException;
+
+
+class Db{
+	
+	private static $pdo =null;
+
+
+	public static function pdo(){
+		if (self::$pdo !== null) {
+			return self::$pdo;
+		}
+		try{
+			$dsn = sprintf('msql:host=%s;dbname=%s;charset=utf8',DB_HOST,DB_NAME);
+			$option = array(PDO::ATTR_DEFAULT_FETCH_MODE =>PDO::FETCH_ASSOC);
+
+			return self::$pdo = new PDO($dns,DB_USER,DB_PASS,$option);
+
+		}catch(PDOException $e){
+			exit($e->getMessage());
+		}
+	}
+}
