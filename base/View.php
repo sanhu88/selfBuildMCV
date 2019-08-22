69ae1164cc4c9c636f85685b35ddf129c4a486f4
Burt Zheng <504779282@qq.com>
2019-08-22 23:06:24 +0800

    part1 of base/view.php

--- base/View.php
+++ base/View.php
@@ -1,1 +1,50 @@
 <?php
+namespace fastphp\base;
+
+/**
+* 			
+*/
+class View
+{
+	protected $variables = array();
+	protected $_controller;
+	protected $_action;
+
+	
+	function __construct($controller,$action)
+	{	
+		$this->_controller = strtolower($controller);
+		$this->_action = strtolower($action);
+	}
+
+	public function assign($name,$value){
+		$this->variables[$name] =$value;
+	}
+
+
+	public function render(){
+		extract($this->variables);
+
+		$defaultHeader = APP_PATH . 'app/views/header.php';
+		$defaultFooter = APP_PATH . 'app/views/footer.php';
+
+		$controllerHeader = APP_PATH . 'app/views/'.$this->_controller.'/header.php';
+		$controllerFooter = APP_PATH . 'app/views/'.$this->_controller.'/footer.php';
+		$controllerLayout = APP_PATH . 'app/views/'.$this->_controller.'/'.$this->_action.'.php';
+
+
+		if (is_file($controllerHeader)) {
+			include($controllerHeader);
+		}else{
+			include($defaultHeader);
+		}
+
+		if (is_file($controllerLayout)) {
+			include($controllerLayout)
+		}else{
+			
+		}
+
+
+	}
+}
