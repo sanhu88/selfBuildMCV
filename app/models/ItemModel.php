f330c8bbbc888d7ed023f1b89c1b86befde82131
Burt Zheng <504779282@qq.com>
2019-08-28 22:43:36 +0800

    complete app/models/ItemModel.php

--- app/models/ItemModel.php
+++ app/models/ItemModel.php
@@ -2,3 +2,18 @@
 namespace app\models;
 
 use fastphp\base\Model;
+use fastphp\base\Db;
+
+
+class ItemModel extends Model{
+	protected $table = 'item';
+
+	public function search($keyword){
+		$sql ="select * from `$this->table` where `item_name` like :keyword";
+		$sth = Db::pdo()->prepare($sql);
+		$sth = $this->formatParam($sth,[':keyword' => "%$keyword%"]);
+		$sth->execute();
+
+		return $sth->fetchAll();
+	}
+}
