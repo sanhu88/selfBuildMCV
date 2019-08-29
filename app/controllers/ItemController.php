3db769820e4153cf170e28b2db3f176a1c84afd7
Burt Zheng <504779282@qq.com>
2019-08-29 22:51:14 +0800

    complete app/controllers/ItemController.php

--- app/controllers/ItemController.php
+++ app/controllers/ItemController.php
@@ -24,4 +24,62 @@ class ItenController extends Controller{
 
 		$this->render();
 	}
+
+
+	public function detail($id){
+
+		$item = (new ItemModel())->where(["id = ?"],[$id])->fetch();
+
+		$this->assign('title','条目具体详情');
+		$this->assign('count',$count);
+		$this->render();
+	}
+
+
+	public function add(){
+
+		$data['item_name'] = $_POST['value'];
+		$count = (new ItemModel)->add($data);//?why not ItemModel()
+
+		$this->assign('title','添加成功');
+		$this->assign('count',$count);
+		$this->render();
+	}
+
+
+	public function manage($id = 0){
+
+		$item = array();
+
+		if ($id) {
+			$item = (new ItemModel())->where(["id = :id"],[':id'=>$id])-fetch();
+		}
+
+		$this->assign('title','管理条目');
+		$this->assign('item',$item);
+		$this->render();
+	}
+
+
+	public function update(){
+
+		$data = array('id'=>$_POST['id'],'item_name'=>$_POST['value']);
+
+		$count = (new ItemModel)->where(['id = :id'],[':id'=>$data['id']])->update($data);//?wh{y not ItemModel()
+		
+				$this->assign('title','修改成功');
+				$this->assign('count',$count);
+				$this->render();
+			}
+		
+		
+			public function delete($id = null){
+
+				$count = (new ItemModel)->delete($id);//?wh{y not ItemModel()
+
+				$this->assign('title','删除成功');
+				$this->assign('count',$count);
+				$this->render();
+
+			}
 }
