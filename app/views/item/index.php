5fd81b2a23713c4f46219ffeb2d0b9b808cc149b
Burt Zheng <504779282@qq.com>
2019-09-01 13:44:47 +0800

    create app/views/item/index.php

---
+++ app/views/item/index.php
@@ -0,0 +1,16 @@
+<form action="" method="get">
+	<input type="text" name="keyword" value="<?php echo $keyword ?>">
+	<input type="submit" name="" value="搜一下">
+</form>
+
+<p><a href="/item/manage">新建</a></p>
+
+<table>
+	<tr>
+		<th>ID</th>
+		<th>内容</th>
+		<th>操作</th>
+	</tr>
+
+	<?php 
+</table>
